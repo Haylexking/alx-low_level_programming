106c0287d86c47db2a633dab923817a65c3e8df9
Alexander Akerele <alexakerele24@gmail.com>
2022-07-01 18:16:49 -0700

    Variables, if, else, and while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - Print all single digit numbers
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int base;
+
+	for (base = 0; base < 10; base++)
+	printf("%d", base);
+	putchar('\n')
+
+	return (0);
+}
