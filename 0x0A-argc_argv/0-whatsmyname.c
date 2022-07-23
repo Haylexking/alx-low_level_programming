950b619bbb8afd1af563d8f2e5f3a18621420591
Alexander Akerele <alexakerele24@gmail.com>
2022-07-22 17:02:15 -0700

    What's my name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 /**
- * main - prints its name, followed by a new line.
- * @argc: number of command line arguments.
- * @argv: array that contains the program command line arguments.
- * Return: 0 - success.
+ * main - prints name
+ * @argc: arguments
+ * @argv: command line arguments
+ * Return: Always 0 to exit
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
+	int x;
+
+	for (x = 0; x < argc; x++)
+	{
+		printf("%s\n", argv[x]);
+	}
 	return (0);
 }
